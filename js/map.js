8e4e8e9600ea6a6f832e732ac84652813d2ee533
Olga Baryshnikova <vasiek1@yandex.ru>
2017-11-27 01:20:15 +0100

    some constants added, functions refactored
    Уже все лучшает.
    
    Вопросы:
    1)не совсем поняла как ты хочешь, чтобы я срендерила features в html.
    Пока оставила свой старый метод, но он по крайней мере теперь работает.
    
    2) вопрос насчет поля address - написала в коде коментом

--- js/map.js
+++ js/map.js
@@ -9,6 +9,24 @@ var OFFERS_INFO = {
 };
 var PIN_HALF_WIDTH = 40 / 2;
 var PIN_HEIGHT = 44;
+var PRICE_MIN_MAX = {
+  min: 1000,
+  max: 1000000
+};
+var ROOMS_GUESTS_MIN_MAX = {
+  min: 1,
+  max: 5
+};
+var LOCATION_MIN_MAX = {
+  x: {
+    min: 300,
+    max: 900
+  },
+  y: {
+    min: 100,
+    max: 500
+  }
+};
 
 var getRandomArbitary = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -17,10 +35,13 @@ var getRandomValue = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-var getFeatures = function () {
+var getRandomArray = function (array) {
   var features = [];
-  for (var i = 0; i < getRandomArbitary(1, 5); i++) {
-    features[i] = OFFERS_INFO.features[getRandomArbitary(1, 5)];
+  for (var i = 0; i < array.length; i++) {
+    var chance = Math.random();
+    if (chance < 0.5) {
+      features.push(array[i]);
+    }
   }
   return features;
 };
@@ -35,55 +56,76 @@ var createNearByArray = function (numberOfOffers, offersInfo) {
         },
         offer: {
           title: getRandomValue(offersInfo.title),
+
+          // Ясно, что это та не работает, но откуда вытащить эти данные? О_О
           // address: nearBy[i].location.x + ' ' + nearBy[i].location.y,
-          price: getRandomArbitary(1000, 1000000),
+          address: 'I am here temporary ^_^',
+          price: getRandomArbitary(PRICE_MIN_MAX.min, PRICE_MIN_MAX.max),
           type: getRandomValue(offersInfo.type),
-          rooms: getRandomArbitary(1, 5),
-          guests: getRandomArbitary(1, 5),
+          rooms: getRandomArbitary(ROOMS_GUESTS_MIN_MAX.min, ROOMS_GUESTS_MIN_MAX.max),
+          guests: getRandomArbitary(ROOMS_GUESTS_MIN_MAX.min, ROOMS_GUESTS_MIN_MAX.max),
           checkin: getRandomValue(offersInfo.checkinout),
           checkout: getRandomValue(offersInfo.checkinout),
-          features: getFeatures(),
+          features: getRandomArray(offersInfo.features),
           description: '',
           photos: []
         },
         location: {
-          x: getRandomArbitary(300, 900) + PIN_HALF_WIDTH,
-          y: getRandomArbitary(100, 500) + PIN_HEIGHT
+          x: getRandomArbitary(LOCATION_MIN_MAX.x.min, LOCATION_MIN_MAX.x.max) + PIN_HALF_WIDTH,
+          y: getRandomArbitary(LOCATION_MIN_MAX.y.min, LOCATION_MIN_MAX.y.max) + PIN_HEIGHT
         }
       };
   }
   return nearBy;
 };
 var nearBy = createNearByArray(NUMBER_OF_OFFERS, OFFERS_INFO);
+
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 var offerTemplate = document.querySelector('template').content.querySelector('.map__card');
+
+var createPin = function () {
+  var pin = pinTemplate.cloneNode(true);
+
+  pin.style.left = nearBy[i].location.x + 'px';
+  pin.style.top = nearBy[i].location.y + 'px';
+  pin.querySelector('img').src = nearBy[i].author.avatar;
+
+  return pin;
+};
+
+var fragment = document.createDocumentFragment();
+
 for (var i = 0; i < nearBy.length; i++) {
-  var getPin = pinTemplate.cloneNode(true);
-
-  getPin.style.left = nearBy[i].location.x + 'px';
-  getPin.style.top = nearBy[i].location.y + 'px';
-  getPin.querySelector('img').src = nearBy[i].author.avatar;
-
-  mapPins.appendChild(getPin);
-}
-var getOffer = offerTemplate.cloneNode(true);
-
-getOffer.querySelector('h3').textContent = nearBy[0].offer.title;
-getOffer.querySelector('small').textContent = nearBy[0].offer.address;
-getOffer.querySelector('.popup__price').textContent = nearBy[0].offer.price + '₽/ночь';
-getOffer.querySelector('h4').textContent = nearBy[0].offer.type[1];
-
-getOffer.querySelector('p:nth-of-type(4)').textContent = 'Заезд после ' + nearBy[0].offer.checkin + ', выезд до ' + nearBy[0].offer.checkout;
-getOffer.querySelector('p:nth-of-type(5)').textContent = nearBy[0].offer.description;
-getOffer.querySelector('img').src = nearBy[0].author.avatar;
-
-// var popupFeatures = document.querySelector('.popup__features');
-for (var j = 0; i < nearBy[0].offer.features.length; j++) {
-  getOffer.querySelector('.popup__features').insertAdjacentHTML('afterbegin', '<li class="features--' + nearBy[0].offer.features[j] + '">test</li>');
+  fragment.appendChild(createPin());
 }
 
-map.appendChild(getOffer);
+mapPins.appendChild(fragment);
+
+var createOffer = function () {
+  var getOffer = offerTemplate.cloneNode(true);
+
+  getOffer.querySelector('h3').textContent = nearBy[0].offer.title;
+  getOffer.querySelector('small').textContent = nearBy[0].offer.address;
+  getOffer.querySelector('.popup__price').textContent = nearBy[0].offer.price + '₽/ночь';
+  getOffer.querySelector('h4').textContent = nearBy[0].offer.type[1];
+  // здесь можно добавить проверку на правильное написания гостя / гостей, комната / комнаты
+  getOffer.querySelector('p:nth-of-type(3)').textContent = nearBy[0].offer.rooms + ' комнаты для ' + nearBy[0].offer.guests + ' гостей';
+  getOffer.querySelector('p:nth-of-type(4)').textContent = 'Заезд после ' + nearBy[0].offer.checkin + ', выезд до ' + nearBy[0].offer.checkout;
+  getOffer.querySelector('p:nth-of-type(5)').textContent = nearBy[0].offer.description;
+  getOffer.querySelector('img').src = nearBy[0].author.avatar;
+
+  getOffer.querySelector('.popup__features').innerHTML = '';
+
+  for (var j = 0; j < nearBy[0].offer.features.length; j++) {
+    getOffer.querySelector('.popup__features').insertAdjacentHTML('afterbegin', '<li class="feature feature--' + nearBy[0].offer.features[j] + '"></li>');
+  }
+
+  return getOffer;
+};
+
+fragment.appendChild(createOffer());
+map.appendChild(fragment);
 
 document.querySelector('.map').classList.remove('map--faded');
