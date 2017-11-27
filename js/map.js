d51345d37829b8507a5f6312ab6beb592d758022
Olga Baryshnikova <vasiek1@yandex.ru>
2017-11-28 00:36:31 +0100

    different fixes
    я не во все коменты въехала

--- js/map.js
+++ js/map.js
@@ -3,7 +3,11 @@
 var NUMBER_OF_OFFERS = 8;
 var OFFERS_INFO = {
   title: ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
-  type: [['flat', 'Квартира'], ['bungalo', 'Бунгало'], ['house', 'Дом']],
+  type: {
+    flat: 'Квартира',
+    bungalo: 'Бунгало',
+    house: 'Дом'
+  },
   checkinout: ['12:00', '13:00', '14:00'],
   features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
 };
@@ -46,10 +50,14 @@ var getRandomArray = function (array) {
   return features;
 };
 
+var getKeyValue = function (obj, key) {
+  return obj[key];
+};
+
 var createNearByArray = function (numberOfOffers, offersInfo) {
   var nearBy = [];
   for (var i = 0; i < numberOfOffers; i++) {
-    var x = getRandomArbitary(LOCATION_MIN_MAX.x.min, LOCATION_MIN_MAX.x.max) + PIN_HALF_WIDTH;
+    var x = getRandomArbitary(LOCATION_MIN_MAX.x.min, LOCATION_MIN_MAX.x.max) - PIN_HALF_WIDTH;
     var y = getRandomArbitary(LOCATION_MIN_MAX.y.min, LOCATION_MIN_MAX.y.max) + PIN_HEIGHT;
     nearBy[i] =
       {
@@ -60,7 +68,7 @@ var createNearByArray = function (numberOfOffers, offersInfo) {
           title: getRandomValue(offersInfo.title),
           address: x + ', ' + y,
           price: getRandomArbitary(PRICE_MIN_MAX.min, PRICE_MIN_MAX.max),
-          type: getRandomValue(offersInfo.type),
+          type: getRandomValue(Object.keys(offersInfo.type)),
           rooms: getRandomArbitary(ROOMS_GUESTS_MIN_MAX.min, ROOMS_GUESTS_MIN_MAX.max),
           guests: getRandomArbitary(ROOMS_GUESTS_MIN_MAX.min, ROOMS_GUESTS_MIN_MAX.max),
           checkin: getRandomValue(offersInfo.checkinout),
@@ -84,12 +92,12 @@ var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 var offerTemplate = document.querySelector('template').content.querySelector('.map__card');
 
-var createPin = function () {
+var createPin = function (array) {
   var pin = pinTemplate.cloneNode(true);
 
-  pin.style.left = nearBy[i].location.x + 'px';
-  pin.style.top = nearBy[i].location.y + 'px';
-  pin.querySelector('img').src = nearBy[i].author.avatar;
+  pin.style.left = array.location.x + 'px';
+  pin.style.top = array.location.y + 'px';
+  pin.querySelector('img').src = array.author.avatar;
 
   return pin;
 };
@@ -97,33 +105,36 @@ var createPin = function () {
 var fragment = document.createDocumentFragment();
 
 for (var i = 0; i < nearBy.length; i++) {
-  fragment.appendChild(createPin());
+  fragment.appendChild(createPin(nearBy[i]));
 }
 
 mapPins.appendChild(fragment);
 
+var firstOffer = nearBy[0];
+
 var createOffer = function () {
   var getOffer = offerTemplate.cloneNode(true);
 
-  getOffer.querySelector('h3').textContent = nearBy[0].offer.title;
-  getOffer.querySelector('small').textContent = nearBy[0].offer.address;
-  getOffer.querySelector('.popup__price').textContent = nearBy[0].offer.price + '₽/ночь';
-  getOffer.querySelector('h4').textContent = nearBy[0].offer.type[1];
-  // оставляю с комнатами, ибо в темплейте так, я думаю в задании просто не дописали комнаты
-  getOffer.querySelector('p:nth-of-type(3)').textContent = nearBy[0].offer.rooms + ' комнаты для ' + nearBy[0].offer.guests + ' гостей';
-  getOffer.querySelector('p:nth-of-type(4)').textContent = 'Заезд после ' + nearBy[0].offer.checkin + ', выезд до ' + nearBy[0].offer.checkout;
-  getOffer.querySelector('p:nth-of-type(5)').textContent = nearBy[0].offer.description;
-  getOffer.querySelector('img').src = nearBy[0].author.avatar;
+  getOffer.querySelector('h3').textContent = firstOffer.offer.title;
+  getOffer.querySelector('small').textContent = firstOffer.offer.address;
+  getOffer.querySelector('.popup__price').textContent = firstOffer.offer.price + '₽/ночь';
+  getOffer.querySelector('h4').textContent = getKeyValue(OFFERS_INFO.type, firstOffer.offer.type);
+  getOffer.querySelector('p:nth-of-type(3)').textContent = firstOffer.offer.rooms + ' комнаты для ' + firstOffer.offer.guests + ' гостей';
+  getOffer.querySelector('p:nth-of-type(4)').textContent = 'Заезд после ' + firstOffer.offer.checkin + ', выезд до ' + firstOffer.offer.checkout;
+  getOffer.querySelector('p:nth-of-type(5)').textContent = firstOffer.offer.description;
+  getOffer.querySelector('img').src = firstOffer.author.avatar;
 
   getOffer.querySelector('.popup__features').innerHTML = '';
 
-  for (var j = 0; j < nearBy[0].offer.features.length; j++) {
-    getOffer.querySelector('.popup__features').insertAdjacentHTML('afterbegin', '<li class="feature feature--' + nearBy[0].offer.features[j] + '"></li>');
+  var featuresHtmlString = '';
+  for (var j = 0; j < firstOffer.offer.features.length; j++) {
+    featuresHtmlString += '<li class="feature feature--' + firstOffer.offer.features[j] + '"></li>';
   }
+  getOffer.querySelector('.popup__features').insertAdjacentHTML('afterbegin', featuresHtmlString);
   return getOffer;
 };
 
 fragment.appendChild(createOffer());
-map.appendChild(fragment);
+map.insertBefore(fragment, map.children[1]);
 
 document.querySelector('.map').classList.remove('map--faded');
